c48b4d832546479b9c020c34c208104e448f8498
darwish1998 <darwish144.ad@gmail.com>
2021-09-09 15:58:51 +0300

    first setup

---
+++ src/components/SignUp.vue
@@ -0,0 +1,52 @@
+<template>
+<img class = 'logo' src="../assets/rest.png">
+<h1>Sign Up</h1>
+
+<div class = 'register'>
+
+  <input type = 'text' placeholder = ' Enter Name'>
+  <input type = 'text' placeholder = ' Enter Email'>
+  <input type = 'password' placeholder = ' Enter password'>
+
+  <button>Sign Up </button>
+
+
+</div>
+
+
+
+
+
+
+</template>
+<script>
+
+export default {
+name: 'SignUp'
+}
+</script>
+<style>
+.logo {
+  width: 300px
+}
+.register input {
+width: 300px;
+height: 40px;
+padding-left: 20px;
+display: block;
+margin-left: auto;
+margin-bottom: 30px;
+margin-right: auto;
+border: 1px solid skyblue;
+}
+
+.register button {
+width:320px;
+height:40px;
+border:1px solid skyblue;
+background:skyblue;
+color: #fff;
+pointer:cursor;
+}
+
+</style>
