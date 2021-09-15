abf9c4c51c717ff300aa59cb796fab808b62dd55
darwish1998 <darwish144.ad@gmail.com>
2021-09-15 08:09:36 +0300

    API complete

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,25 +4,48 @@
 
 <div class = 'register'>
 
-  <input type = 'text' placeholder = ' Enter Name'>
-  <input type = 'text' placeholder = ' Enter Email'>
-  <input type = 'password' placeholder = ' Enter password'>
+  <input type = 'text' v-model = "name" placeholder = ' Enter Name'/>
+  <input type = 'text' v-model = "email" placeholder = ' Enter Email'/>
+  <input type = 'password' v-model = "password" placeholder = ' Enter password'/>
 
-  <button>Sign Up </button>
+  <button v-on:click="SignUp">Sign Up </button>
 
 
 </div>
 
-
-
-
-
-
 </template>
 <script>
+import axios from 'axios'
 
 export default {
-name: 'SignUp'
+name: 'SignUp',
+  data()
+  {
+  return {
+  email:'',
+  name:'',
+  password:''
+
+  }
+  },
+  methods:{
+  async  SignUp()
+  {
+    let result = await  axios.post('http://localhost:3000/users',{
+    email:this.email,
+    password:this.password,
+    name:this.name
+    });
+    console.warn(result);
+    if(result.status == 201)
+    {
+      alert("SignUp is complete");
+      localStorage.setItem("user-info",JSON.stringify(result.data))
+    }
+
+  }
+  }
+
 }
 </script>
 <style>
@@ -46,7 +69,7 @@ height:40px;
 border:1px solid skyblue;
 background:skyblue;
 color: #fff;
-pointer:cursor;
+cursor: pointer;
 }
 
 </style>
